c40278e092800a37359417bea37006d3c00c785a
jnguy707 <Jeremy.N.707@live.com>
2021-12-10 14:18:32 -0800

    added ng component login-form & added routing functionality & outlining user.service

--- front-p2/src/app/login-form/login-form.component.ts
+++ front-p2/src/app/login-form/login-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = this.formBuilder.group({
+    email: [''],
+    password: ['']
+  });
+
+  handleSubmit(event: Event) {
+    let credentials = this.loginForm.value;
+    console.log(credentials);
+    // this.userService.doLogin(credentials)
+    //   .subscribe({
+    //     next: (response: any) => {
+    //       localStorage.setItem("token", response.jwt);
+    //       this.router.navigate([/*some url*/])
+    //     }
+    //   })
+  }
+
+  constructor(private formBuilder: FormBuilder, 
+              private userService: UserService,
+              private router: Router) { 
+  }
 
   ngOnInit(): void {
   }
