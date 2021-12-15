fda0bdb7cb1c10ffb1adfd8c5996505368cbbb0f
jcolebot <jcthewriter@gmail.com>
2021-12-15 17:58:53 -0500

    Adding UserController and payload/HttpResponseBody

---
+++ back-end/src/main/java/com/revature/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.revature.backend.controller;
+
+import com.revature.backend.controller.payload.HttpResponseBody;
+import com.revature.backend.entity.User;
+import com.revature.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/users"
+    )
+    public ResponseEntity<?> doPost(@RequestBody User user) {
+        userService.register(user);
+        HttpResponseBody responseBody = new HttpResponseBody("user registered");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+    }
+}
---
+++ back-end/src/main/java/com/revature/backend/controller/payload/HttpResponseBody.java
@@ -0,0 +1,13 @@
+package com.revature.backend.controller.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HttpResponseBody {
+    private String message;
+}
