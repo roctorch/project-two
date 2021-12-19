67e5ce33bde88bbe7a0d2c1b4ccc300acc96545c
jnguy707 <Jeremy.N.707@live.com>
2021-12-19 13:37:49 -0800

    updated auth-guard link and initialized schedule.service

---
+++ front-p2/src/app/schedule.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  constructor() { }
+}
